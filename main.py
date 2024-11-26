67474be364a96d393a80f3283a44fefc880f2766
Romann <dess8de@gmail.com>
2024-11-26 17:44:57 +0300

    update

--- main.py
+++ main.py
@@ -7,21 +7,17 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from fluentogram import TranslatorHub
-from aiogram_i18n import I18nContext, I18nMiddleware, LazyProxy
-from aiogram_i18n.cores.fluent_compile_core import FluentCompileCore
 
 from config_data.config import Config, load_config
 from handlers.user import user_router
 from middlewares.i18n import TranslatorRunnerMiddleware
-from middlewares.CallbackMiddleware import CallbackMiddleware
-from middlewares import i18nMiddleware
 from utils.i18n import create_translator_hub
 
 
 logging.basicConfig(
     level=logging.DEBUG,
     format='[%(asctime)s] #%(levelname)-8s %(filename)s:'
-            '%(lineno)d - %(name)s - %(message)s'
+    '%(lineno)d - %(name)s - %(message)s'
 )
 
 logger = logging.getLogger(__name__)
